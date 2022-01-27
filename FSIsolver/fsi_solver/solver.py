afa5fa5453f9b562d708c1acb78fde3b9e292287
JohannesHaubner <haubnerj@simula.no>
2022-01-27 11:36:46 +0100

    fix 1st bug

--- FSIsolver/fsi_solver/solver.py
+++ FSIsolver/fsi_solver/solver.py
@@ -100,14 +100,14 @@ class FSI(Context):
                 ui.vector()[:] = -1.0 * u.vector()[:]
                 pmed = assemble(p * self.dxf)
                 vol = assemble(Constant("1.0") * self.dxf)
-                ALE.move(self.mesh, u)
+                ALE.move(self.mesh, u, annotate=False)
                 pp = project(p - pmed / vol * Constant("1.0"), p.function_space())
                 v.rename("velocity", "velocity")
                 p.rename("pressure", "pressure")
                 self.pfile << p
                 self.vfile << v
 
-                ALE.move(self.mesh, ui)
+                ALE.move(self.mesh, ui, annotate=False)
 
                 # save characteristic function of solid mesh
                 Vs = VectorFunctionSpace(self.FSI_params["solid_mesh"], "CG", 2)
@@ -116,9 +116,9 @@ class FSI(Context):
                 us = transfer_to_subfunc(u, Vs)
                 usi = Function(us.function_space())
                 usi.vector()[:] = -1.0 * us.vector()[:]
-                ALE.move(self.FSI_params["solid_mesh"], us)
+                ALE.move(self.FSI_params["solid_mesh"], us, annotate=False)
                 self.cfile << c
-                ALE.move(self.FSI_params["solid_mesh"], usi)
+                ALE.move(self.FSI_params["solid_mesh"], usi, annotate=False)
 
 
     def save_displacement(self, u):
---
+++ example/example_learnext_FSIbenchmarkII.py
@@ -0,0 +1,111 @@
+from dolfin import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+import sys
+sys.path.insert(1, '../FSIsolver/extension_operator')
+import extension
+sys.path.insert(1, '../FSIsolver/fsi_solver')
+import solver
+sys.path.insert(1, '../learnExt')
+from NeuralNet.neural_network_custom import ANN, generate_weights
+
+# create mesh: first create mesh by running ./create_mesh/create_mesh_FSI.py
+
+# load mesh
+mesh = Mesh()
+with XDMFFile("./../Output/Mesh_Generation/mesh_triangles.xdmf") as infile:
+    infile.read(mesh)
+mvc = MeshValueCollection("size_t", mesh, 2)
+mvc2 = MeshValueCollection("size_t", mesh, 2)
+with XDMFFile("./../Output/Mesh_Generation/facet_mesh.xdmf") as infile:
+    infile.read(mvc, "name_to_read")
+with XDMFFile("./../Output/Mesh_Generation/mesh_triangles.xdmf") as infile:
+    infile.read(mvc2, "name_to_read")
+boundaries = cpp.mesh.MeshFunctionSizet(mesh, mvc)
+domains = cpp.mesh.MeshFunctionSizet(mesh,mvc2)
+bdfile = File("./../Output/Mesh_Generation/boundary.pvd")
+bdfile << boundaries
+bdfile = File("./../Output/Mesh_Generation/domains.pvd")
+bdfile << domains
+
+# boundary parts
+params = np.load('../Output/Mesh_Generation/params.npy', allow_pickle='TRUE').item()
+
+params["no_slip_ids"] = ["noslip", "obstacle_fluid", "obstacle_solid"]
+
+# subdomains
+fluid_domain = MeshView.create(domains, params["fluid"])
+solid_domain = MeshView.create(domains, params["solid"])
+#plot(solid_domain)
+#plt.show()
+
+# parameters for FSI system
+FSI_param = {}
+
+FSI_param['fluid_mesh'] = fluid_domain
+FSI_param['solid_mesh'] = solid_domain
+
+FSI_param['lambdas'] = 2.0e6
+FSI_param['mys'] = 0.5e6
+FSI_param['rhos'] = 1.0e4
+FSI_param['rhof'] = 1.0e3
+FSI_param['nyf'] = 1.0e-3
+
+FSI_param['t'] = 0.0
+FSI_param['deltat'] = 0.0025
+FSI_param['T'] = 15.0
+
+FSI_param['displacement_point'] = Point((0.6, 0.2))
+
+# boundary conditions, need to be 0 at t = 0
+Ubar = 1.0
+FSI_param['boundary_cond'] = Expression(("(t < 2)?(1.5*Ubar*4.0*x[1]*(0.41 -x[1])/ 0.1681*0.5*(1-cos(pi/2*t))):"
+                                         "(1.5*Ubar*4.0*x[1]*(0.41 -x[1]))/ 0.1681", "0.0"),
+                                        Ubar=Ubar, t=FSI_param['t'], degree=2)
+
+# extension operator
+class LearnExtension(extension.ExtensionOperator):
+    def __init__(self, mesh):
+        super().__init__(mesh)
+
+        T = VectorElement("CG", self.mesh.ufl_cell(), 1)
+        self.FS = FunctionSpace(self.mesh, T)
+        output_directory = str("../Output/learnExt/results/")
+        self.net = ANN(output_directory + "trained_network.pkl")
+
+    def extend(self, boundary_conditions):
+        """ biharmonic extension of boundary_conditions (Function on self.mesh) to the interior """
+
+        u = Function(self.FS)
+        v = TestFunction(self.FS)
+
+        dx = Measure('dx', domain=self.mesh)
+
+        E = inner(
+            self.net(inner(1 / 2 * (grad(u) + grad(u).T), 1 / 2 * (grad(u) + grad(u).T))) * 1 / 2
+            * (grad(u) + grad(u).T), 1 / 2 * (grad(v) + grad(v).T)) * dx(self.mesh)
+
+        # solve PDE
+        bc = DirichletBC(self.FS, boundary_conditions, 'on_boundary')
+
+
+        solve(E == 0, u, bc)
+
+        save_ext = False
+        if save_ext:
+            file = File('../../Output/Extension/function.pvd')
+            file << u
+
+        return u
+
+extension_operator = LearnExtension(fluid_domain)
+
+# save options
+FSI_param['save_directory'] = str('./../Output/FSIbenchmarkII') #no save if set to None
+FSI_param['save_every_N_snapshot'] = 16 # save every 8th snapshot
+
+# initialize FSI solver
+fsisolver = solver.FSIsolver(mesh, boundaries, domains, params, FSI_param, extension_operator)
+fsisolver.solve()
+
