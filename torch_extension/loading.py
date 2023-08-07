e6da0b51e96f9b6344ec1e8576d8cba3fe508ee1
Ottar Hellan <hellanottar@simula.no>
2023-08-07 19:26:45 +0000

    Updated model loading.

--- torch_extension/loading.py
+++ torch_extension/loading.py
@@ -7,65 +7,79 @@ import yaml
 from typing import Literal
 from os import PathLike
 
-from torch_extension.networks import MLP, Normalizer
+import networks
 
 class ModelBuilder:
 
-    def __new__(cls, model_dict: dict) -> nn.Module:
-
-        assert len(model_dict.keys()) == 1
-        key = next(iter(model_dict.keys()))
-        val = model_dict[key]
-
-        model: nn.Module = getattr(cls, key)(val)
-
-        return model
-    
     def activation(activation_type: str) -> nn.Module:
         activations = {"ReLU": nn.ReLU(), 
                        "Tanh": nn.Tanh(),
                        "Sigmoid": nn.Sigmoid()}
         return activations[activation_type]
 
-    def MLP(mlp_dict: dict) -> MLP:
+    def MLP(mlp_dict: dict) -> networks.MLP:
 
         activation = ModelBuilder.activation(mlp_dict["activation"])
         widths = mlp_dict["widths"]
 
-        return MLP(widths, activation)
+        return networks.MLP(widths, activation)
     
     def Sequential(model_dicts: list[dict]) -> nn.Sequential:
 
-        return nn.Sequential(*(ModelBuilder(model_dict)
+        return nn.Sequential(*(build_model(model_dict)
                                 for model_dict in model_dicts))
     
-    def Normalizer(normalizer_dict: dict) -> Normalizer:
+    def Normalizer(normalizer_dict: dict) -> networks.Normalizer:
 
         x_mean = torch.Tensor(normalizer_dict["x_mean"])
-        x_std = torch.Tensor(normalizer_dict["x_std"])
+        x_var = torch.Tensor(normalizer_dict["x_var"])
 
-        return Normalizer(x_mean, x_std)
+        return networks.Normalizer(x_mean, x_var)
+    
+    def InverseNormalizer(normalizer_dict: dict) -> networks.InverseNormalizer:
+
+        x_mean = torch.Tensor(normalizer_dict["x_mean"])
+        x_var = torch.Tensor(normalizer_dict["x_var"])
+
+        return networks.InverseNormalizer(x_mean, x_var)
 
 
 class ModelLoader:
 
     def __new__(cls, model_dir: PathLike, load_state_dict: bool = True,
                 mode: Literal["yaml", "json"] = "yaml") -> nn.Module:
-        model_dir = Path(model_dir)
-        if not model_dir.is_dir():
-            raise ValueError("Non-existent directory.")
+        
+        return load_model(model_dir, load_state_dict=load_state_dict, mode=mode)
 
-        if mode == "yaml":
-            with open(model_dir / "model.yml", "r") as infile:
-                model_dict = yaml.safe_load(infile.read())
-        elif mode == "json":
-            with open(model_dir / "model.json", "r") as infile:
-                model_dict= json.loads(infile.read())
-        else:
-            raise ValueError
-        model: nn.Module = ModelBuilder(model_dict)
 
-        if load_state_dict:
-            model.load_state_dict(torch.load(model_dir / "state_dict.pt"))
+def build_model(model_dict: dict) -> nn.Module:
 
-        return model
+    assert len(model_dict.keys()) == 1
+    key = next(iter(model_dict.keys()))
+    val = model_dict[key]
+
+    model: nn.Module = getattr(ModelBuilder, key)(val)
+
+    return model
+
+
+def load_model(model_dir: PathLike, load_state_dict: bool = True,
+                            mode: Literal["yaml", "json"] = "yaml") -> nn.Module:
+    model_dir = Path(model_dir)
+    if not model_dir.is_dir():
+        raise ValueError("Non-existent directory.")
+
+    if mode == "yaml":
+        with open(model_dir / "model.yml", "r") as infile:
+            model_dict = yaml.safe_load(infile.read())
+    elif mode == "json":
+        with open(model_dir / "model.json", "r") as infile:
+            model_dict= json.loads(infile.read())
+    else:
+        raise ValueError
+    model = build_model(model_dict)
+
+    if load_state_dict:
+        model.load_state_dict(torch.load(model_dir / "state_dict.pt"))
+
+    return model
