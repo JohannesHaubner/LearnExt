29cbe0faec0d74b1f33ef0e27c8727c703f616bf
Ottar Hellan <hellanottar@simula.no>
2023-11-24 12:23:18 +0000

    Removing old unstable explicit solvers.

--- gravity_driven_test/solver.py
+++ gravity_driven_test/solver.py
@@ -24,7 +24,7 @@ def get_S(E, mu: df.Constant, lambda_: df.Constant):
 class Solver:
 
     def __init__(self, problem: Problem, order: int = 2, dt: float = 1e-3, T: float = 0.1,
-                 time_stepping: Literal["explicit_euler", "IMEX", "step_implicit_euler"] = "implicit_euler"):
+                 time_stepping: Literal["implicit_euler"] = "implicit_euler"):
 
         self.problem = problem
 
@@ -40,8 +40,6 @@ class Solver:
 
         self.time_stepping = time_stepping
         self.step = {
-            "explicit_euler": self.step_explicit_euler,
-            "IMEX": self.step_IMEX,
             "implicit_euler": self.step_implicit_euler
         }[self.time_stepping]
 
@@ -57,7 +55,6 @@ class Solver:
 
         w = df.Function(self.W)
         self.u_, self.v_ = w.split()
-
 
         return
     
@@ -80,70 +77,6 @@ class Solver:
         solid_file.close()
 
         return
-    
-
-    def step_explicit_euler(self):
-        
-        u, v, y, z = self.u, self.v, self.y, self.z
-        u_, v_ = self.u_, self.v_
-
-        rho = df.Constant(self.problem.rho)
-        mu = df.Constant(self.problem.mu)
-        lambda_ = df.Constant(self.problem.lambda_)
-        g = df.Constant((0.0, self.problem.l))
-        k = self.k
-        
-        F = df.Identity(2) + df.grad(u_)
-        E = 0.5 * (F.T * F - df.Identity(2))
-        S = 2*mu * E + lambda_ * df.tr(E) * df.Identity(2)
-        P = F * S
-
-
-        lhs = df.inner(u, y) * df.dx + \
-              rho * df.inner(v, z) * df.dx
-
-        rhs = df.inner(u_, y) * df.dx + k * df.inner(v_, y) * df.dx + \
-              rho * k * df.inner(g, z) * df.dx  -k * df.inner(P, df.grad(z)) * df.dx
-        
-        bc = df.DirichletBC(self.W.sub(0), df.Constant((0.0, 0.0)), self.boundaries, 4)
-
-        w = df.Function(self.W)
-        df.solve(lhs == rhs, w, [bc])
-        self.u_, self.v_ = w.split()
-
-        return
-    
-    def step_IMEX(self):
-
-        u, v, y, z = self.u, self.v, self.y, self.z
-        u_, v_ = self.u_, self.v_
-
-        rho = df.Constant(self.problem.rho)
-        mu = df.Constant(self.problem.mu)
-        lambda_ = df.Constant(self.problem.lambda_)
-        g = df.Constant((0.0, self.problem.l))
-        k = self.k
-        
-        F = df.Identity(2) + df.grad(u_)
-        E = 0.5 * (F.T * F - df.Identity(2))
-        S = 2*mu * E + lambda_ * df.tr(E) * df.Identity(2)
-        P = F * S
-
-
-        lhs = df.inner(u, y) * df.dx - k * df.inner(v, y) * df.dx + \
-              rho * df.inner(v, z) * df.dx
-
-        rhs = df.inner(u_, y) * df.dx + \
-              rho * k * df.inner(g, z) * df.dx  -k * df.inner(P, df.grad(z)) * df.dx
-        
-        bc = df.DirichletBC(self.W.sub(0), df.Constant((0.0, 0.0)), self.boundaries, 4)
-
-        w = df.Function(self.W)
-        df.solve(lhs == rhs, w, [bc])
-        self.u_, self.v_ = w.split()
-
-        return
-    
 
     def step_implicit_euler(self):
 
