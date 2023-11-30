009d2cabf71f9c61ac7cc91ca156d3b2ec6412af
Ottar Hellan <hellanottar@simula.no>
2023-11-30 12:42:02 +0000

    Rollout for hybrid and hybrid incremental. Timing nn-corrected extension and made fix to make it approx 5x as fast.

--- rollout_hybrid_inc.py
+++ rollout_hybrid_inc.py
@@ -70,9 +70,22 @@ import FSIsolver.extension_operator.extension as extension
 from learnExt.NeuralNet.neural_network_custom import ANN, generate_weights
 from learnExt.learnext_hybridPDENN import Custom_Reduced_Functional as crf
 
+
+# extension operator
 class LearnExtension(extension.ExtensionOperator):
     def __init__(self, mesh):
         super().__init__(mesh)
+
+        # T = df.VectorElement("CG", self.mesh.ufl_cell(), 1)
+        # T2 = df.VectorElement("CG", self.mesh.ufl_cell(), 2)
+        # self.FS = df.FunctionSpace(self.mesh, T)
+        # self.FS2 = df.FunctionSpace(self.mesh, T2)
+        # self.incremental = False
+        # self.incremental_correct = False
+        # self.bc_old = df.Function(self.FS)
+        # output_directory = str(str(here.parent) + "/example/learned_networks/")
+        # self.net = ANN(output_directory + "trained_network_supervised.pkl")
+        # self.threshold
 
         T = df.VectorElement("CG", self.mesh.ufl_cell(), 1)
         T2 = df.VectorElement("CG", self.mesh.ufl_cell(), 2)
@@ -83,43 +96,99 @@ class LearnExtension(extension.ExtensionOperator):
         self.net = ANN(network_path)
         self.threshold = 0.001
 
+        self.first = True
+
+
+
     def extend(self, boundary_conditions, params = None):
         """ harmonic extension of boundary_conditions (Function on self.mesh) to the interior """
 
+        if params != None:
+            try:
+                b_old = params["b_old"]
+            except:
+                pass
+            try:
+                displacementy = params["displacementy"]
+            except:
+                displacementy = None
+
+        # if self.incremental == True and self.incremental_correct == False:
+        #     trafo = True
+        # elif self.incremental == True and self.incremental_correct == True:
+        # if displacementy == None:
+        #     Warning("displacementy == None; set trafo to False")
+        #     trafo = False
+        # elif abs(displacementy) <= 0.005: 
+        if abs(displacementy) <= 0.005 and False:
+            # print('displacementy <= 0.005: displacementy = ', displacementy)
+            trafo = False
+        elif self.first:
+            trafo = False
+            self.first = False
+        else:
+            trafo = True
+        # else:
+        #     trafo = False
+
+
+        # if b_old != None:
+        # self.bc_old = df.project(b_old, self.FS)
 
         u = df.Function(self.FS2)
         v = df.TestFunction(self.FS2)
 
+
+        if trafo:
+            up = df.project(self.bc_old, self.FS)
+            upi = df.project(-1.0*up, self.FS)
+            df.ALE.move(self.mesh, up, annotate=False)
+
         dx = df.Measure('dx', domain=self.mesh, metadata={'quadrature_degree': 4})
 
-        E = df.inner(crf.NN_der(self.threshold, df.inner(df.grad(u), df.grad(u)), self.net) * df.grad(u), df.grad(v)) * dx
-
+        if trafo:
+            E = df.inner(crf.NN_der(self.threshold, df.inner(df.grad(self.bc_old), df.grad(self.bc_old)), self.net) * df.grad(u), df.grad(v)) * dx
+        
+        else:
+            E = df.inner(crf.NN_der(self.threshold, df.inner(df.grad(u), df.grad(u)), self.net) * df.grad(u), df.grad(v)) * dx
 
         # solve PDE
-        bc = df.DirichletBC(self.FS2, boundary_conditions, 'on_boundary')
+        if trafo:
+            bc_func = df.project(boundary_conditions - self.bc_old, self.FS2)
+        else:
+            bc_func = boundary_conditions
+
+        bc = df.DirichletBC(self.FS2, bc_func, 'on_boundary')
 
 
         df.solve(E == 0, u, bc, solver_parameters={"nonlinear_solver": "newton", "newton_solver":
             {"maximum_iterations": 200}})
 
+        if trafo:
+            u = df.project(u + self.bc_old, self.FS2)
+
 
         self.bc_old.assign(df.project(u, self.FS))
+
+        if trafo:
+            df.ALE.move(self.mesh, upi, annotate=False)
 
         return u
 
 extension_operator = LearnExtension(fluid_domain)
 
 
+
 input_xdmf_file = df.XDMFFile("Output/Extension/Data/input_.xdmf")
-if Path("hybridRolloutP1.xdmf").exists():
-    Path("hybridRolloutP1.xdmf").unlink()
-    Path("hybridRolloutP1.h5").unlink
-# if Path("hybridRolloutP2.xdmf").exists():
-#     Path("hybridRolloutP2.xdmf").unlink()
-#     Path("hybridRolloutP2.h5").unlink
-output_xdmf_file_p1 = df.XDMFFile("hybridRolloutP1.xdmf")
+if Path("hybridIncRolloutP1.xdmf").exists():
+    Path("hybridIncRolloutP1.xdmf").unlink()
+    Path("hybridIncRolloutP1.h5").unlink
+# if Path("hybridIncRolloutP2.xdmf").exists():
+#     Path("hybridIncRolloutP2.xdmf").unlink()
+#     Path("hybridIncRolloutP2.h5").unlink
+output_xdmf_file_p1 = df.XDMFFile("hybridIncRolloutP1.xdmf")
 output_xdmf_file_p1.write(fluid_domain)
-# output_xdmf_file_p2 = df.XDMFFile("hybridRolloutP2.xdmf")
+# output_xdmf_file_p2 = df.XDMFFile("hybridIncRolloutP2.xdmf")
 # output_xdmf_file_p2.write(fluid_domain)
 
 df.set_log_active(False)
@@ -128,7 +197,8 @@ u_p1 = df.Function(fspace)
 disp_point = df.Point((0.6, 0.2))
 for k in tqdm.tqdm(range(206)): # Number of time steps that make it loop best.
     input_xdmf_file.read_checkpoint(u_bc, "input_harmonic_ext", k)
-    disp_y = u_bc(disp_point)
+    # disp_y = u_bc(disp_point)[1]
+    disp_y = 0.1
     u_ext = extension_operator.extend(u_bc, {"displacementy": disp_y, "b_old": None})
     u_p1.interpolate(u_ext)
     # output_xdmf_file_p2.write_checkpoint(u_ext, "uh", k, append=True)
--- torch_extension/extension.py
+++ torch_extension/extension.py
@@ -289,6 +289,111 @@ class TorchExtensionInplace(extension.ExtensionOperator):
 
         return self.u_
 
+class TorchExtensionInplaceMat(extension.ExtensionOperator):
+
+    def __init__(self, mesh, model: nn.Module, T_switch: float = 0.0, mask_rhs: str | None = None, silent: bool = False):
+        super().__init__(mesh)
+
+        T = df.VectorElement("CG", self.mesh.ufl_cell(), 2)
+        self.F = df.FunctionSpace(self.mesh, T)
+        self.iter = -1
+
+        # harmonic extension 
+        uh = df.TrialFunction(self.F)
+        v = df.TestFunction(self.F)
+
+        a = df.inner(df.grad(uh), df.grad(v))*df.dx
+        L = df.Constant(0.0) * v[0] *df.dx
+        A = df.assemble(a)
+        
+        bc = df.DirichletBC(self.F, df.Constant((0.,0.)), 'on_boundary')
+        bc.apply(A)
+
+        self.solver_harmonic = df.LUSolver(A)
+        self.rhs_harmonic = df.assemble(L)
+
+        # For clement interpolation
+        T_cg1 = df.VectorElement("CG", self.mesh.ufl_cell(), 1)
+        self.F_cg1 = df.FunctionSpace(self.mesh, T_cg1)
+
+        # u_cg1 is referred to in the clement_interpolate internals, so it has to 
+        # be carried through in the simulation and the dofs updated with the 
+        # harmonic extension at each time step.
+        self.u_cg1 = df.Function(self.F_cg1)
+        _, self.clement_interpolater = clement_interpolate(df.grad(self.u_cg1), with_CI=True)
+
+        # Pytorch model
+        self.model = model
+        model.eval()
+
+        # mask for adjusting pytorch correction
+        V_scal = df.FunctionSpace(self.mesh, "CG", 1)
+        if mask_rhs is None:
+            # Masking function custom made for specific domain.
+            mask_rhs = "2.0 * (x[0]+1.0) * (1-x[0]) * exp( -3.5*pow(x[0], 7) ) + 0.1"
+        poisson_mask = poisson_mask_custom(V_scal, mask_rhs, normalize=True)
+        self.mask_np = poisson_mask.vector().get_local().reshape(-1,1)
+        # Mask needs to have shape (num_vertices, 1) to broadcast correctly in
+        # multiplication with correction of shape (num_vertices, 2).
+
+        # # Time to switch from harmonic to torch-corrected extension
+        self.T_switch = T_switch
+
+        self.silent = silent
+
+        self.harm_cg1 = df.Function(self.F_cg1)
+        self.uh = df.Function(self.F)
+        self.u_ = df.Function(self.F)
+
+        self.interp_mat_2_1 = df.PETScDMCollection.create_transfer_matrix(self.F, self.F_cg1)
+        self.interp_mat_1_2 = df.PETScDMCollection.create_transfer_matrix(self.F_cg1, self.F)
+
+        return
+
+    def extend(self, boundary_conditions, params):
+        """ Torch-corrected extension of boundary_conditions (Function on self.mesh) to the interior """
+
+        t = params["t"]
+
+        # harmonic extension
+        bc = df.DirichletBC(self.F, boundary_conditions, 'on_boundary')
+        bc.apply(self.rhs_harmonic)
+        self.solver_harmonic.solve(self.uh.vector(), self.rhs_harmonic)
+
+        if t < self.T_switch:
+            self.u_ = self.uh
+        
+        else:
+            if not self.silent:
+                print("Torch-corrected extension")
+
+            # self.harm_cg1.interpolate(self.uh)
+            self.interp_mat_2_1.mult(self.uh.vector(), self.harm_cg1.vector())
+
+            self.u_cg1.vector().set_local(self.harm_cg1.vector().get_local())
+            gh_harm = self.clement_interpolater()
+
+            harmonic_plus_grad_w_coords_np = CG1_vector_plus_grad_to_array_w_coords(self.harm_cg1, gh_harm)
+            harmonic_plus_grad_w_coords_torch = torch.tensor(harmonic_plus_grad_w_coords_np, dtype=torch.float32)
+
+            with torch.no_grad():
+                corr_np = self.model(harmonic_plus_grad_w_coords_torch).numpy()
+                corr_np = corr_np * self.mask_np
+
+            new_dofs = self.harm_cg1.vector().get_local()
+            new_dofs[0::2] += corr_np[:,0]
+            new_dofs[1::2] += corr_np[:,1]
+            self.harm_cg1.vector().set_local(new_dofs)
+
+            # self.u_.interpolate(self.harm_cg1)
+            self.interp_mat_1_2.mult(self.harm_cg1.vector(), self.u_.vector())
+
+            # Apply the harmonic extension boundary condition to ensure fluid-solid extension matches at all
+            # dof locations, not just vertices.
+            bc.apply(self.u_.vector())
+
+        return self.u_
+
 class TorchExtensionRecord(TorchExtension):
     def __init__(self, mesh, model, T_switch=0.0, mask_rhs = None, T_record=0.0, run_name="Data0"):
         super().__init__(mesh, model, T_switch=T_switch, mask_rhs=mask_rhs)
